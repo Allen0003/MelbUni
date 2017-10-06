ed8b0fccdfeb686968f9e1399b46eb9acb5995aa
Allen0003 <apss1943@gmail.com>
2017-10-06 15:45:49 +1100

    Add files via upload

--- src/JavaClass/assignment/Poker.java
+++ src/JavaClass/assignment/Poker.java
@@ -5,41 +5,61 @@ import java.util.Collections;
 
 import JavaClass.assignment.properties.Card;
 import JavaClass.assignment.properties.Player;
+import JavaClass.assignment.util.Const;
 import JavaClass.assignment.util.UtilImpl;
+
+/**
+ * <h1>main method of Poker project</h1>
+ * <p>
+ * handle string from args and then set it to each player
+ * </p>
+ * 
+ * @author Allen
+ * @loginId wenpinw
+ * @version 1.0
+ * @since 04-10-2017
+ * @param args
+ * 
+ */
 
 public class Poker {
 	public static void main(String[] args) {
-		// String input = "KS 9S QS TS 2S KD KC KS KH 4S KS 9C QS TS 2S";
-		String input = "KS 9C QS TS 2S  KS 9C QS TS 2S  KS 9C QS TS 2S";
-		ArrayList<Card> cards = new ArrayList<Card>();
-		String[] temp = input.split("  ");
+
+		String input = "";
+		for (String temp : args) {
+			input += temp;
+		}
+		input = input.replaceAll("..(?!$)", "$0 ");
+
+		String[] temp = input.toUpperCase().split(" ");
 		UtilImpl util = new UtilImpl();
 		ArrayList<Player> players = new ArrayList<Player>();
 		try {
+			if (temp.length % 5 != 0) {
+				throw new Exception(Const.ERROR5);
+			}
 			int number = 0;
+			int cardIndex = 0;
+			Player player = null;
 			for (String inputs : temp) {
-				String[] cards_s = inputs.split(" ");
-				Player player = new Player();
-				cards = new ArrayList<Card>();
-				for (String card_s : cards_s) {
-					Card card = util.setCard(card_s);
-					cards.add(card);
+				if (cardIndex % 5 == 0) {
+					player = new Player();
+					player.setCards(new ArrayList<Card>());
+					player.setNumber(++number);
+					
+					players.add(player);
 				}
-				if (cards.size() != 5) {
-					throw new Exception("wrong number of arguments; must be a multiple of 5");
-				}
-				player.setCards(cards);
-				player.setNumber(++number);
-				players.add(player);
+				player.getCards().add(util.setCard(inputs));
+				cardIndex++;
 			}
 			for (int i = 0; i < players.size(); i++) {
 				Collections.sort(players.get(i).getCards());
 				util.setRule(players.get(i));
-				System.out.println("player = " + i);
-				System.out.println(" my result = " + players.get(i).getResult());
+				System.out.println("Player " + players.get(i).getNumber() + ": " + players.get(i).getResult());
 			}
-
-			System.out.println(util.printFinalResult(players));
+			if (players.size() != 1) {
+				System.out.println(util.printFinalResult(players));
+			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
